4e4c0f1203dd11e09871644d8e51f7c9f1ce89be
nolash <dev@holbrook.no>
2021-06-04 10:54:10 +0200

    Complete refactor basic tests

--- python/tests/test_basic.py
+++ python/tests/test_basic.py
@@ -25,7 +25,6 @@ testdir = os.path.dirname(__file__)
 
 class TestBasic(TestDemurrageDefault):
 
-    @unittest.skip('foo')
     def test_hello(self):
 
         nonce_oracle = RPCNonceOracle(self.accounts[0], self.rpc)
@@ -38,7 +37,6 @@ class TestBasic(TestDemurrageDefault):
         r = self.rpc.do(o)
 
 
-    @unittest.skip('foo')
     def test_apply_demurrage(self):
         modifier = 10 * (10 ** 37)
 
@@ -66,7 +64,6 @@ class TestBasic(TestDemurrageDefault):
         self.assertEqual(modifier, demurrage_amount)
 
 
-    @unittest.skip('foo')
     def test_mint(self):
         nonce_oracle = RPCNonceOracle(self.accounts[0], self.rpc)
         c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
@@ -102,7 +99,6 @@ class TestBasic(TestDemurrageDefault):
         self.assertEqual(balance, int(2000 * 0.98))
 
 
-    @unittest.skip('foo')
     def test_minter_control(self):
 
         nonce_oracle = RPCNonceOracle(self.accounts[1], self.rpc)
@@ -155,7 +151,6 @@ class TestBasic(TestDemurrageDefault):
         self.assertEqual(r['status'], 0)
             
 
-
     def test_base_amount(self):
         nonce_oracle = RPCNonceOracle(self.accounts[0], self.rpc)
         c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
@@ -172,51 +167,77 @@ class TestBasic(TestDemurrageDefault):
         self.assertEqual(amount, 1020)
 
 
-#    def test_transfer(self):
-#        tx_hash = self.contract.functions.mintTo(self.w3.eth.accounts[1], 1024).transact()
-#        r = self.w3.eth.getTransactionReceipt(tx_hash)
-#        self.assertEqual(r.status, 1)
-#
-#        tx_hash = self.contract.functions.transfer(self.w3.eth.accounts[2], 500).transact({'from': self.w3.eth.accounts[1]})
-#        r = self.w3.eth.getTransactionReceipt(tx_hash)
-#        self.assertEqual(r.status, 1)
-#        logg.debug('tx {}'.format(r))
-#
-#        balance_alice = self.contract.functions.balanceOf(self.w3.eth.accounts[1]).call()
-#        self.assertEqual(balance_alice, 524)
-#
-#        balance_bob = self.contract.functions.balanceOf(self.w3.eth.accounts[2]).call()
-#        self.assertEqual(balance_bob, 500)
-#
-#        tx_hash = self.contract.functions.transfer(self.w3.eth.accounts[2], 500).transact({'from': self.w3.eth.accounts[1]})
-#        r = self.w3.eth.getTransactionReceipt(tx_hash)
-#        self.assertEqual(r.status, 1)
-#        logg.debug('tx {}'.format(r))
-#
-#
-#    def test_transfer_from(self):
-#        tx_hash = self.contract.functions.mintTo(self.w3.eth.accounts[1], 1024).transact()
-#        r = self.w3.eth.getTransactionReceipt(tx_hash)
-#        self.assertEqual(r.status, 1)
-#
-#        tx_hash = self.contract.functions.approve(self.w3.eth.accounts[2], 500).transact({'from': self.w3.eth.accounts[1]})
-#        r = self.w3.eth.getTransactionReceipt(tx_hash)
-#        self.assertEqual(r.status, 1)
-#        logg.debug('tx {}'.format(r))
-#
-#        balance_alice = self.contract.functions.balanceOf(self.w3.eth.accounts[1]).call()
-#        self.assertEqual(balance_alice, 1024)
-#
-#        tx_hash = self.contract.functions.transferFrom(self.w3.eth.accounts[1], self.w3.eth.accounts[3], 500).transact({'from': self.w3.eth.accounts[2]})
-#        r = self.w3.eth.getTransactionReceipt(tx_hash)
-#        self.assertEqual(r.status, 1)
-#        logg.debug('tx {}'.format(r))
-#
-#        balance_alice = self.contract.functions.balanceOf(self.w3.eth.accounts[1]).call()
-#        self.assertEqual(balance_alice, 524)
-#
-#        balance_alice = self.contract.functions.balanceOf(self.w3.eth.accounts[3]).call()
-#        self.assertEqual(balance_alice, 500)
+    def test_transfer(self):
+        nonce_oracle = RPCNonceOracle(self.accounts[0], self.rpc)
+        c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+
+        (tx_hash, o) = c.mint_to(self.address, self.accounts[0], self.accounts[1], 1024)
+        self.rpc.do(o)
+
+        nonce_oracle = RPCNonceOracle(self.accounts[1], self.rpc)
+        c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        (tx_hash, o) = c.transfer(self.address, self.accounts[1], self.accounts[2], 500)
+        self.rpc.do(o)
+        o = receipt(tx_hash)
+        r = self.rpc.do(o)
+        self.assertEqual(r['status'], 1)
+
+        o = c.balance_of(self.address, self.accounts[1], sender_address=self.accounts[0])
+        r = self.rpc.do(o)
+        balance = c.parse_balance_of(r)
+        self.assertEqual(balance, 524)
+
+        o = c.balance_of(self.address, self.accounts[2], sender_address=self.accounts[0])
+        r = self.rpc.do(o)
+        balance = c.parse_balance_of(r)
+        self.assertEqual(balance, 500)
+
+        nonce_oracle = RPCNonceOracle(self.accounts[2], self.rpc)
+        c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        (tx_hash, o) = c.transfer(self.address, self.accounts[2], self.accounts[1], 500)
+        self.rpc.do(o)
+        o = receipt(tx_hash)
+        r = self.rpc.do(o)
+        self.assertEqual(r['status'], 1)
+
+       
+
+    def test_transfer_from(self):
+        nonce_oracle = RPCNonceOracle(self.accounts[0], self.rpc)
+        c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        (tx_hash, o) = c.mint_to(self.address, self.accounts[0], self.accounts[1], 1024)
+        self.rpc.do(o)
+
+        nonce_oracle = RPCNonceOracle(self.accounts[1], self.rpc)
+        c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        (tx_hash, o) = c.approve(self.address, self.accounts[1], self.accounts[2], 500)
+        self.rpc.do(o)
+        o = receipt(tx_hash)
+        r = self.rpc.do(o)
+        self.assertEqual(r['status'], 1)
+        
+        o = c.balance_of(self.address, self.accounts[1], sender_address=self.accounts[0])
+        r = self.rpc.do(o)
+        balance = c.parse_balance_of(r)
+        self.assertEqual(balance, 1024)
+
+        nonce_oracle = RPCNonceOracle(self.accounts[2], self.rpc)
+        c = DemurrageToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        (tx_hash, o) = c.transfer_from(self.address, self.accounts[2], self.accounts[1], self.accounts[3], 500)
+        self.rpc.do(o)
+        o = receipt(tx_hash)
+        r = self.rpc.do(o)
+        self.assertEqual(r['status'], 1)
+ 
+        o = c.balance_of(self.address, self.accounts[1], sender_address=self.accounts[0])
+        r = self.rpc.do(o)
+        balance = c.parse_balance_of(r)
+        self.assertEqual(balance, 524)
+
+        o = c.balance_of(self.address, self.accounts[3], sender_address=self.accounts[0])
+        r = self.rpc.do(o)
+        balance = c.parse_balance_of(r)
+        self.assertEqual(balance, 500)
 
 
 if __name__ == '__main__':
